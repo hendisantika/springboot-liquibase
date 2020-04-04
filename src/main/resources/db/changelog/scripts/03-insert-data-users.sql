f01ad04f9b82f0d68692ca7f5a1b117bf275da3f
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-05 05:37:25 +0700

    #7 - Add 03-insert-data-users.sql file

---
+++ src/main/resources/db/changelog/scripts/03-insert-data-users.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERS(ID, NAME, EMAIL, ADDRESS)
+VALUES (1, 'Soumitra', 'soumitra@email.com', 1);
+INSERT INTO USERS(ID, NAME, EMAIL, PHONE, ADDRESS)
+VALUES (2, 'Suman', 'suman@email.com', 1254789541, 2);
+
